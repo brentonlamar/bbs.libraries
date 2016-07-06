ca513865320c3bba79b8ea14190272688bcb1c76
Brenton Reeder <brenton.reeder@betabytesoftware.com>
2016-07-06 16:21:48 -0400

    Adding build flag and version

--- Scripts/DeployingNuget.sh
+++ Scripts/DeployingNuget.sh
@@ -13,17 +13,17 @@ if [ "$TRAVIS_BRANCH" == "adding-nuget" ]; then
 
 	echo "Starting to pack the NuGet packages"
 
-	nuget pack ./BBS.Libraries.Contracts/BBS.Libraries.Contracts.nuspec
-	nuget pack ./BBS.Libraries.Emails/BBS.Libraries.Emails.nuspec
-	nuget pack ./BBS.Libraries.Enums/BBS.Libraries.Enums.nuspec
-	nuget pack ./BBS.Libraries.Extensions/BBS.Libraries.Extensions.nuspec
-	nuget pack ./BBS.Libraries.IO/BBS.Libraries.IO.nuspec
-	nuget pack ./BBS.Libraries.Logging/BBS.Libraries.Logging.nuspec
-	nuget pack ./BBS.Libraries.Logging.AppInsights/BBS.Libraries.Logging.AppInsights.nuspec
-	nuget pack ./BBS.Libraries.SQL/BBS.Libraries.SQL.nuspec
-	nuget pack ./BBS.Libraries.Templating/BBS.Libraries.Templating.nuspec
-	nuget pack ./BBS.Libraries.Templating.Handlebars/BBS.Libraries.Templating.Handlebars.nuspec
-	nuget pack ./BBS.Libraries.Templating.Razor/BBS.Libraries.Templating.Razor.nuspec
+	nuget pack ./BBS.Libraries.Contracts/BBS.Libraries.Contracts.nuspec -Build
+	nuget pack ./BBS.Libraries.Emails/BBS.Libraries.Emails.nuspec -Build
+	nuget pack ./BBS.Libraries.Enums/BBS.Libraries.Enums.nuspec -Build
+	nuget pack ./BBS.Libraries.Extensions/BBS.Libraries.Extensions.nuspec -Build
+	nuget pack ./BBS.Libraries.IO/BBS.Libraries.IO.nuspec -Build
+	nuget pack ./BBS.Libraries.Logging/BBS.Libraries.Logging.nuspec -Build
+	nuget pack ./BBS.Libraries.Logging.AppInsights/BBS.Libraries.Logging.AppInsights.nuspec -Build
+	nuget pack ./BBS.Libraries.SQL/BBS.Libraries.SQL.nuspec -Build
+	nuget pack ./BBS.Libraries.Templating/BBS.Libraries.Templating.nuspec -Build
+	nuget pack ./BBS.Libraries.Templating.Handlebars/BBS.Libraries.Templating.Handlebars.nuspec -Build
+	nuget pack ./BBS.Libraries.Templating.Razor/BBS.Libraries.Templating.Razor.nuspec -Build
 
 	echo "Finished packing."
 
