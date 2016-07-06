467f44429045f7cccfef48810dce674075bc8d27
Brenton Reeder <brenton.reeder@betabytesoftware.com>
2016-07-06 16:34:20 -0400

    Updating versioning

--- Scripts/DeployingNuget.sh
+++ Scripts/DeployingNuget.sh
@@ -1,29 +1,41 @@
 # Gets Version from AsseblyInfo.cs updates nuspec's version
 if [ "$TRAVIS_BRANCH" == "adding-nuget" ]; then
-	#declare regex="AssemblyVersion\(\"([0-9]+.[0-9]+.[0-9]+.[0-9]+)\"\)"
+	declare regex="AssemblyVersion\(\"([0-9]+.[0-9]+.[0-9]+.[0-9]+)\"\)"
 	#declare file_content=$( cat $AssemblyFilePath)
 
-	#if [[ " $file_content " =~ $regex ]] # please note the space before and after the file content
-	#	then
+	if [[ " $file_content " =~ $regex ]] # please note the space before and after the file content
+		then
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.Contracts/BBS.Libraries.Contracts.nuspec 
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.Emails/BBS.Libraries.Emails.nuspec 
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.Enums/BBS.Libraries.Enums.nuspec 
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.Extensions/BBS.Libraries.Extensions.nuspec 
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.IO/BBS.Libraries.IO.nuspec 
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.Logging/BBS.Libraries.Logging.nuspec 
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.Logging.AppInsights/BBS.Libraries.Logging.AppInsights.nuspec 
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.SQL/BBS.Libraries.SQL.nuspec 
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.Templating/BBS.Libraries.Templating.nuspec 
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.Templating.Handlebars/BBS.Libraries.Templating.Handlebars.nuspec 
+			sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" ./BBS.Libraries.Templating.Razor/BBS.Libraries.Templating.Razor.nuspec 
+
 	#		sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1${BASH_REMATCH[1]}\2|g" $ProjectName.nuspec
-	#	else
-	#		echo "Have not updated version of NuGet Package"
-	#		exit 1
-	#fi
+		else
+			echo "Have not updated version of NuGet Package"
+			exit 1
+	fi
 
 	echo "Starting to pack the NuGet packages"
 
-	nuget pack ./BBS.Libraries.Contracts/BBS.Libraries.Contracts.nuspec -Build
-	nuget pack ./BBS.Libraries.Emails/BBS.Libraries.Emails.nuspec -Build
-	nuget pack ./BBS.Libraries.Enums/BBS.Libraries.Enums.nuspec -Build
-	nuget pack ./BBS.Libraries.Extensions/BBS.Libraries.Extensions.nuspec -Build
-	nuget pack ./BBS.Libraries.IO/BBS.Libraries.IO.nuspec -Build
-	nuget pack ./BBS.Libraries.Logging/BBS.Libraries.Logging.nuspec -Build
-	nuget pack ./BBS.Libraries.Logging.AppInsights/BBS.Libraries.Logging.AppInsights.nuspec -Build
-	nuget pack ./BBS.Libraries.SQL/BBS.Libraries.SQL.nuspec -Build
-	nuget pack ./BBS.Libraries.Templating/BBS.Libraries.Templating.nuspec -Build
-	nuget pack ./BBS.Libraries.Templating.Handlebars/BBS.Libraries.Templating.Handlebars.nuspec -Build
-	nuget pack ./BBS.Libraries.Templating.Razor/BBS.Libraries.Templating.Razor.nuspec -Build
+	nuget pack ./BBS.Libraries.Contracts/BBS.Libraries.Contracts.nuspec 
+	nuget pack ./BBS.Libraries.Emails/BBS.Libraries.Emails.nuspec 
+	nuget pack ./BBS.Libraries.Enums/BBS.Libraries.Enums.nuspec 
+	nuget pack ./BBS.Libraries.Extensions/BBS.Libraries.Extensions.nuspec 
+	nuget pack ./BBS.Libraries.IO/BBS.Libraries.IO.nuspec 
+	nuget pack ./BBS.Libraries.Logging/BBS.Libraries.Logging.nuspec 
+	nuget pack ./BBS.Libraries.Logging.AppInsights/BBS.Libraries.Logging.AppInsights.nuspec 
+	nuget pack ./BBS.Libraries.SQL/BBS.Libraries.SQL.nuspec 
+	nuget pack ./BBS.Libraries.Templating/BBS.Libraries.Templating.nuspec 
+	nuget pack ./BBS.Libraries.Templating.Handlebars/BBS.Libraries.Templating.Handlebars.nuspec 
+	nuget pack ./BBS.Libraries.Templating.Razor/BBS.Libraries.Templating.Razor.nuspec 
 
 	echo "Finished packing."
 
