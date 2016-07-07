c44d313346f1ad2ff9fe8fd7150571218cb10a69
Brenton Reeder <brenton.reeder@betabytesoftware.com>
2016-07-07 08:41:17 -0400

    Version variable not right, hardcoding the pattern for now

--- Scripts/DeployingNuget.sh
+++ Scripts/DeployingNuget.sh
@@ -1,20 +1,20 @@
 # Gets Version from AsseblyInfo.cs updates nuspec's version
 if [ "$TRAVIS_BRANCH" == "adding-nuget" ]; then
-	declare $versionNumber = "1.0.${TRAVIS_BUILD_NUMBER}.0"
+	# Need to make this work => declare $versionNumber = "1.0.${TRAVIS_BUILD_NUMBER}.0"
 	echo "========================================================="
 	echo "Updating nuspec version"
 
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.Contracts/BBS.Libraries.Contracts.nuspec
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.Emails/BBS.Libraries.Emails.nuspec 
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.Enums/BBS.Libraries.Enums.nuspec 
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.Extensions/BBS.Libraries.Extensions.nuspec 
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.IO/BBS.Libraries.IO.nuspec 
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.Logging/BBS.Libraries.Logging.nuspec 
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.Logging.AppInsights/BBS.Libraries.Logging.AppInsights.nuspec 
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.SQL/BBS.Libraries.SQL.nuspec   
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.Templating/BBS.Libraries.Templating.nuspec 
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.Templating.Handlebars/BBS.Libraries.Templating.Handlebars.nuspec  
-	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\1$versionNumber\2|g" ./BBS.Libraries.Templating.Razor/BBS.Libraries.Templating.Razor.nuspec  
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.Contracts/BBS.Libraries.Contracts.nuspec
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.Emails/BBS.Libraries.Emails.nuspec 
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.Enums/BBS.Libraries.Enums.nuspec 
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.Extensions/BBS.Libraries.Extensions.nuspec 
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.IO/BBS.Libraries.IO.nuspec 
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.Logging/BBS.Libraries.Logging.nuspec 
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.Logging.AppInsights/BBS.Libraries.Logging.AppInsights.nuspec 
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.SQL/BBS.Libraries.SQL.nuspec   
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.Templating/BBS.Libraries.Templating.nuspec 
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.Templating.Handlebars/BBS.Libraries.Templating.Handlebars.nuspec  
+	sed -i "s|\(<version>\)[^<>]*\(</version>\)|\11.0.${TRAVIS_BUILD_NUMBER}.0\2|g" ./BBS.Libraries.Templating.Razor/BBS.Libraries.Templating.Razor.nuspec  
 
 	echo "========================================================="
 
@@ -36,15 +36,15 @@ if [ "$TRAVIS_BRANCH" == "adding-nuget" ]; then
 
 	echo "Deploying to public hosting server"	
 
-	nuget push ./BBS.Libraries.Contracts.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
-	nuget push ./BBS.Libraries.Emails.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
-	nuget push ./BBS.Libraries.Enums.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
-	nuget push ./BBS.Libraries.Extensions.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
-	nuget push ./BBS.Libraries.IO.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
-	nuget push ./BBS.Libraries.Logging.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
-	nuget push ./BBS.Libraries.Logging.AppInsights.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
-	nuget push ./BBS.Libraries.SQL.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
-	nuget push ./BBS.Libraries.Templating.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
-	nuget push ./BBS.Libraries.Templating.Handlebars.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
-	nuget push ./BBS.Libraries.Templating.Razor.$versionNumber.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.Contracts.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.Emails.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.Enums.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.Extensions.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.IO.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.Logging.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.Logging.AppInsights.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.SQL.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.Templating.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.Templating.Handlebars.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
+	nuget push ./BBS.Libraries.Templating.Razor.1.0.${TRAVIS_BUILD_NUMBER}.0.nupkg $PublicNugetAPIKey -Source https://www.nuget.org/api/v2/package
 fi
